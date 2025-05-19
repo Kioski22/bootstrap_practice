d87478745c8fbd32c108d892808788d5f9780c68
kioski21 <152053430+kioski21@users.noreply.github.com>
2025-05-20 00:04:26 +0800

    paturo

--- Login.php
+++ Login.php
@@ -1,4 +1,4 @@
-<?
+<?php
 require ('db.php');
 ?>
 
@@ -8,9 +8,9 @@ require ('db.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log in Page</title>
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/bootstrap-grid.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <style>
@@ -23,33 +23,32 @@ require ('db.php');
         input{
             max-width: fit-content;
         }
-
     </style>
 </head>
 
 <body>
     <div class="nav-container">
-            <ul class="nav nav-pills gap-2 p-2 small bg-primary rounded-5 shadow-sm" role="tablist" style="--bs-nav-link-color: var((--bs-white); --bs-nav-pills-link-active-color: var(--bs-primary); --bs-nav-pills-link-active-bg: var(--bs-white);">
-                <li class="btn btn-primary " id="profile-tab"><button class="nav-link rounded-5 text-white" id="profile-tab2" data-bs-toggle="tab" type="button" role="tab" aria-selected="false" >Profile</button>
-  </li>
-            </ul>
+        <ul class="nav nav-pills gap-2 p-2 small bg-primary rounded-5 shadow-sm" role="tablist" style="--bs-nav-link-color: var(--bs-white); --bs-nav-pills-link-active-color: var(--bs-primary); --bs-nav-pills-link-active-bg: var(--bs-white);">
+            <li class="btn btn-primary" id="profile-tab">
+                <button class="nav-link rounded-5 text-white" id="profile-tab2" data-bs-toggle="tab" type="button" role="tab" aria-selected="false">Profile</button>
+            </li>
+        </ul>
     </div>
-<div class="container">
-    <div class="row">
-        <div class="col-11 lg-col-6 d-flex justify-content-center">
-            <h1 class="text-center"></h1>
-            <label for="loginForm">Username</label>
-            <input type="text" placeholder="Juan123" id="loginForm" class="form-control">
+    <div class="container">
+        <div class="row">
+            <div class="col-11 col-lg-6 d-flex justify-content-center">
+                <form method="post" action="login_process.php" class="w-100">
+                    <h1 class="text-center">Login</h1>
+                    <label for="username">Username</label>
+                    <input type="text" placeholder="Juan123" id="username" name="username" class="form-control">
 
-            <label for="loginForm" class="label">Password</label>
-            <input type="password" class="form-control" >
+                    <label for="password" class="label">Password</label>
+                    <input type="password" id="password" name="password" class="form-control">
 
+                    <button class="btn btn-success mt-3" type="submit">Login</button>
+                </form>
+            </div>
         </div>
     </div>
-</div>
-
-
-
-
 </body>
 </html>
