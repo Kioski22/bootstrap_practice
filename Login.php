b7fe6c0d0df8bbc371a6a47c139c3aa252bca5e3
kioski21 <152053430+kioski21@users.noreply.github.com>
2025-05-21 01:48:52 +0800

    di to okay

--- Login.php
+++ Login.php
@@ -13,9 +13,11 @@ require ('db.php');
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/bootstrap-grid.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-    <style>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+   <style>
         body{
             font-family: Poppins, Helvetica, sans-serif !important;
+            background-color: #948979 !important;
          
         }
 
@@ -23,20 +25,59 @@ require ('db.php');
             max-width: fit-content;
         }
         #LoginForm{
-            background-color: burlywood;
+            background-color: #222831;
             margin-left: 500px;
             padding: 200px;
             border-radius: 20px !important;
         }
+        ul{
+            background-color: #393E46 ;
+        }
+       .profile-tab{
+        background-color: #DFD0B8;
+       }
+       .link{
+        text-decoration: none;
+        color: white;
+       }
+       .profile-tab{
+        margin: 20px;
+        transition: ease-in;
+        transition-duration: 1s;
+       }
+       .profile-tab : :hover{
+        background: #DFD0B8;
+        
+       } 
+     
+     
     </style>
 </head>
 
 <body>
     <div class="nav-container">
-        <ul class="nav nav-pills gap-2 p-2 small bg-primary rounded-5 shadow-sm" role="tablist" style="--bs-nav-link-color: var(--bs-white); --bs-nav-pills-link-active-color: var(--bs-primary); --bs-nav-pills-link-active-bg: var(--bs-white);">
-            <li class="btn btn-primary" id="profile-tab">
-                <button class="nav-link rounded-5 text-white" id="profile-tab2" data-bs-toggle="tab" type="button" role="tab" aria-selected="false">Profile</button>
+       
+        <ul class="nav nav-pills gap-2 p-2 small   shadow-sm" role="tablist" style="--bs-nav-link-color: var(--bs-white); --bs-nav-pills-link-active-color: var(--bs-primary); --bs-nav-pills-link-active-bg: var(--bs-white);">
+            <li class="btn " id="profile-tab">
+                <button class="nav-link rounded-5 text-white" id="profile-tab1" data-bs-toggle="tab" type="button" role="tab" aria-selected="false"><a href="#" class="link">Home</a></button>
             </li>
+
+              <li class="btn " id="profile-tab">
+                <button class="nav-link rounded-5 text-white" id="profile-tab1" data-bs-toggle="tab" type="button" role="tab" aria-selected="false"><a href="#" class="link">About</a></button>
+            </li>
+
+              <li class="btn " id="profile-tab">
+                <button class="nav-link rounded-5 text-white" id="profile-tab1" data-bs-toggle="tab" type="button" role="tab" aria-selected="false"><a href="#" class="link">Product</a></button>
+            </li>
+
+              <li class="btn " id="profile-tab">
+                <button class="nav-link rounded-5 text-white" id="profile-tab1" data-bs-toggle="tab" type="button" role="tab" aria-selected="false"><a href="#" class="link">About Us</a></button>
+            </li>
+            <li class="btn" id="profile-tab">
+            <a href="" class="logo-link"><i class="fa fa-user  aria-hidden="true" id="logo-dropdown"></i></a>
+           </li> 
+            
+          
         </ul>
     </div>
     <div class="container">
